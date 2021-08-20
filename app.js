991b7e047de3867a44723a3dbe57c1f1e9c9e9e9
sos2020-hash <qweiop525@gmail.com>
2021-08-20 13:45:52 +0800

    work home school btn description update

--- app.js
+++ app.js
@@ -1,109 +1,197 @@
 // sidebar interaction
-const toggleBtn = document.querySelector('.sidebar-toggle')
-const closeBtn = document.querySelector('.close-btn')
-const sidebar = document.querySelector('.sidebar')
+const toggleBtn = document.querySelector(".sidebar-toggle");
+const closeBtn = document.querySelector(".close-btn");
+const sidebar = document.querySelector(".sidebar");
 
-toggleBtn.addEventListener('click', () => {
-    sidebar.classList.toggle('show-sidebar')
-})
+toggleBtn.addEventListener("click", () => {
+  sidebar.classList.toggle("show-sidebar");
+});
 
-closeBtn.addEventListener('click', () => {
-    sidebar.classList.remove('show-sidebar');
-})
-
-//todo list interaciton
+closeBtn.addEventListener("click", () => {
+  sidebar.classList.remove("show-sidebar");
+});
 
 //selecter
-const showButton = document.getElementById('show-btn');
-
-//get function
-async function showData() {
-    const displayDataArea = document.getElementById('display-data-area')
-    const res = await fetch('http://localhost:8080/todolist');
-    const data = await res.json();
-    for (let i = 0; i < data.length; i++) {
-        displayDataArea.innerHTML += `
-        <form id='data-detail-form' class="open-btn">
-            <div id='data-${i}' class='data-field'>
-            <div class='id'>${data[i].id}</div>
-            <div class="name">${data[i].name}</div>
-            <div class="assigned-to">${data[i].assignedto}</div>
-            <div class="due-date">${data[i].duedate}</div>
-            <div class="status">${data[i].status}</div>
-            <button type="button" class="button update" id="${data[i].id}">UPDATE</button>
-            <button type="button" class="button delete" id="${data[i].id}">DELETE</button>
+const showButton = document.getElementById("show-btn");
+const displayDataArea = document.getElementById("display-data-area");
+//Work btn
+const workBtn = document.getElementById("work-btn");
+workBtn.addEventListener("click", async () => {
+  const res = await fetch("http://localhost:8080/todolist");
+  const data = await res.json();
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].assignedto === "work") {
+      displayDataArea.innerHTML = `
+        <form id='data-detail-form'>
+        <div id='data-${i}' class='data-field'>
+        <div class='id'>${data[i].id}</div>
+        <div class="name">${data[i].name}</div>
+        <div class="assigned-to">${data[i].assignedto}</div>
+        <div class="due-date">${data[i].duedate}</div>
+        <div class="status">${data[i].status}</div>
+            <button type="button" class="button update" id="${data[i].id}"><i class="fas fa-edit"></i></button>
+            <button type="button" class="button delete" id="${data[i].id}"><i class="far fa-trash-alt"></i></button>
+            <button type="button" class="button plus" id="${data[i].id}"><i class="far fa-plus-square"></i></button>
             </div>
         </form>
-        <form id='data-detail-form' class="hidden description-container">
-        <div class="description">${data[i].description}</div></form>`
+        <form id='data-detail-form' class="description-container">
+        <div class="description">${data[i].description}</div></form>`;
     }
+  }
+});
+//Home btn
+const homeBtn = document.getElementById("home-btn");
+homeBtn.addEventListener("click", async () => {
+  const res = await fetch("http://localhost:8080/todolist");
+  const data = await res.json();
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].assignedto === "home") {
+      displayDataArea.innerHTML = `
+        <form id='data-detail-form'>
+        <div id='data-${i}' class='data-field'>
+        <div class='id'>${data[i].id}</div>
+        <div class="name">${data[i].name}</div>
+        <div class="assigned-to">${data[i].assignedto}</div>
+        <div class="due-date">${data[i].duedate}</div>
+        <div class="status">${data[i].status}</div>
+            <button type="button" class="button update" id="${data[i].id}"><i class="fas fa-edit"></i></button>
+            <button type="button" class="button delete" id="${data[i].id}"><i class="far fa-trash-alt"></i></button>
+            <button type="button" class="button plus" id="${data[i].id}"><i class="far fa-plus-square"></i></button>
+            </div>
+        </form>
+        <form id='data-detail-form' class="description-container">
+        <div class="description">${data[i].description}</div></form>`;
+    }
+  }
+});
+//School btn
+const schoolBtn = document.getElementById("school-btn");
+schoolBtn.addEventListener("click", async () => {
+  const res = await fetch("http://localhost:8080/todolist");
+  const data = await res.json();
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].assignedto === "school") {
+      displayDataArea.innerHTML = `
+        <form id='data-detail-form'>
+        <div id='data-${i}' class='data-field'>
+        <div class='id'>${data[i].id}</div>
+        <div class="name">${data[i].name}</div>
+        <div class="assigned-to">${data[i].assignedto}</div>
+        <div class="due-date">${data[i].duedate}</div>
+        <div class="status">${data[i].status}</div>
+            <button type="button" class="button update" id="${data[i].id}"><i class="fas fa-edit"></i></button>
+            <button type="button" class="button delete" id="${data[i].id}"><i class="far fa-trash-alt"></i></button>
+            <button type="button" class="button plus" id="${data[i].id}"><i class="far fa-plus-square"></i></button>
+            </div>
+        </form>
+        <form id='data-detail-form' class="description-container">
+        <div class="description">${data[i].description}</div></form>`;
+    }
+  }
+});
+//get function
+async function showData() {
+  //   const displayDataArea = document.getElementById("display-data-area");
+  const res = await fetch("http://localhost:8080/todolist");
+  const data = await res.json();
+  for (let i = 0; i < data.length; i++) {
+    displayDataArea.innerHTML += `
+        <form id='data-detail-form'>
+        <div id='data-${i}' class='data-field'>
+        <div class='id'>${data[i].id}</div>
+        <div class="name">${data[i].name}</div>
+        <div class="assigned-to">${data[i].assignedto}</div>
+        <div class="due-date">${data[i].duedate}</div>
+        <div class="status">${data[i].status}</div>
+            <button type="button" class="button update" id="${data[i].id}"><i class="fas fa-edit"></i></button>
+            <button type="button" class="button delete" id="${data[i].id}"><i class="far fa-trash-alt"></i></button>
+            <button type="button" class="button plus" id="${data[i].id}"><i class="far fa-plus-square"></i></button>
+            </div>
+        </form>
+        <form id='data-detail-form' class="description-container">
+        <div class="description">${data[i].description}</div></form>`;
+  }
+  //open button
 
-    const updateButtons = document.querySelectorAll('.button.update')
-    for (let updateButton of updateButtons) {
-        updateButton.addEventListener('click', () => {
-            updateItem(updateButton.id)
-        })
+  const openes = document.querySelectorAll(".button");
+  const descriptiones = document.querySelectorAll(".description-container");
+  openes.forEach(function (btn) {
+    if (btn.classList.contains("plus")) {
+      btn.addEventListener("click", function () {
+        descriptiones.forEach(function (description) {
+          description.classList.toggle("show-text");
+        });
+      });
     }
-    const deleteButtons = document.querySelectorAll('.button.delete')
-    for (let deleteButton of deleteButtons) {
-        deleteButton.addEventListener('click', () => {
-            deleteItem(deleteButton.id)
-        })
-    }
+  });
+
+  //updateBtn
+  const updateButtons = document.querySelectorAll(".button.update");
+  for (let updateButton of updateButtons) {
+    updateButton.addEventListener("click", () => {
+      updateItem(updateButton.id);
+    });
+  }
+  //deleteBtn
+  const deleteButtons = document.querySelectorAll(".button.delete");
+  for (let deleteButton of deleteButtons) {
+    deleteButton.addEventListener("click", () => {
+      deleteItem(deleteButton.id);
+    });
+  }
 }
 
 //show listener
-showButton.addEventListener('click', showData);
+showButton.addEventListener("click", showData);
 
-
-document.querySelector('#add-btn').addEventListener('click', () => {
-    if (document.querySelector('#input-data-area').style.display === 'none') {
-        document.querySelector('#input-data-area').style.display = 'flex'
-    } else {
-        document.querySelector('#input-data-area').style.display = 'none'
-    };
-})
+document.querySelector("#add-btn").addEventListener("click", () => {
+  if (document.querySelector("#input-data-area").style.display === "none") {
+    document.querySelector("#input-data-area").style.display = "flex";
+  } else {
+    document.querySelector("#input-data-area").style.display = "none";
+  }
+});
 
 //add function
-document.querySelector('#data-form').addEventListener('submit', async (event) => {
+document
+  .querySelector("#data-form")
+  .addEventListener("submit", async (event) => {
     event.preventDefault();
-    const form = event.target
+    const form = event.target;
     const dataObj = {
-    id: form.id.value,
-    name: form.name.value,
-    description: form.description.value,
-    assignedto: form.assignedto.value,
-    duedate: form.duedate.value,
-    status: form.status.value
-    }
+      id: form.id.value,
+      name: form.name.value,
+      description: form.description.value,
+      assignedto: form.assignedto.value,
+      duedate: form.duedate.value,
+      status: form.status.value,
+    };
 
-
-    const res = await fetch('http://localhost:8080/todolist',
-        {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(dataObj)
-        })
+    const res = await fetch("http://localhost:8080/todolist", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(dataObj),
+    });
     if (res.ok) {
-        console.log(await res.json())
-        showData()
+      console.log(await res.json());
+      // showData()
     }
-})
+  });
 
 //update function
 const updateItem = async (id) => {
-    let res = await fetch('http://localhost:8080/todolist')
-    let selectedItem = {};
-    let updatedItem = {};
-    let resArr = await res.json()
-    for (let resItem of resArr){
-        if (resItem.id === id){
-            selectedItem = {...resItem}
-        }
+  let res = await fetch("http://localhost:8080/todolist");
+  let selectedItem = {};
+  let updatedItem = {};
+  let resArr = await res.json();
+  for (let resItem of resArr) {
+    if (resItem.id === id) {
+      selectedItem = { ...resItem };
     }
-    document.querySelector('#display-data-area').innerHTML = `
+  }
+  document.querySelector("#display-data-area").innerHTML = `
     <form id='update-form'>
     <input type='text' name='id' value="${selectedItem.id}">
     <input type='text' name='name' value="${selectedItem.name}">
@@ -113,36 +201,36 @@ const updateItem = async (id) => {
     <input type='text' name='status' value="${selectedItem.status}">
     <button class='button'>UPDATE</button>
     </form>
-    `
-    document.querySelector('#update-form').addEventListener('submit', (event) => {
-        event.preventDefault();
-        updatedItem.id = event.target.id.value
-        updatedItem.name = event.target.name.value
-        updatedItem.description = event.target.description.value
-        updatedItem.assignedto = event.target.assignedto.value
-        updatedItem.duedate = event.target.duedate.value
-        updatedItem.status = event.target.status.value
-        performUpdate(updatedItem)
-    })
-}
+    `;
+  document.querySelector("#update-form").addEventListener("submit", (event) => {
+    event.preventDefault();
+    updatedItem.id = event.target.id.value;
+    updatedItem.name = event.target.name.value;
+    updatedItem.description = event.target.description.value;
+    updatedItem.assignedto = event.target.assignedto.value;
+    updatedItem.duedate = event.target.duedate.value;
+    updatedItem.status = event.target.status.value;
+    performUpdate(updatedItem);
+  });
+};
 
 const performUpdate = async (data) => {
-    let dataObj = {
-        id: data.id,
-        name: data.name,
-        description: data.description,
-        assignedto: data.assignedto,
-        duedate: data.duedate,
-        status: data.status
-    }
-    const url = 'http://localhost:8080/todolist/' + data.id
-    let res = await fetch(url, {
-        method: "PUT",
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify(dataObj)
-    })
-    if(res.ok){
-        document.querySelector('#display-data-area').innerHTML = `
+  let dataObj = {
+    id: data.id,
+    name: data.name,
+    description: data.description,
+    assignedto: data.assignedto,
+    duedate: data.duedate,
+    status: data.status,
+  };
+  const url = "http://localhost:8080/todolist/" + data.id;
+  let res = await fetch(url, {
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(dataObj),
+  });
+  if (res.ok) {
+    document.querySelector("#display-data-area").innerHTML = `
         <div>Item ${data.id} is updated</div>
         <div id='data-${data.id}' class='data-field'>
         <div class='id'>ID: ${data.id}</div>
@@ -152,32 +240,23 @@ const performUpdate = async (data) => {
         <div class="due-date">DUE Date: ${data.duedate}</div>
         <div class="status">Status: ${data.status}</div>
         </div>
-        `
-    }
-}
+        `;
+  }
+};
 
 const deleteItem = async (id) => {
-    const url = 'http://localhost:8080/todolist/' + id
-    const setting = {
-        method: 'DELETE'
-    }
-    const res = await fetch(url, setting)
-    if(res.ok){
-        document.querySelector('#display-data-area').innerHTML = "<div> item: " + id +" is deleted.</div>"
-    }
-}
+  const url = "http://localhost:8080/todolist/" + id;
+  const setting = {
+    method: "DELETE",
+  };
+  const res = await fetch(url, setting);
+  if (res.ok) {
+    document.querySelector("#display-data-area").innerHTML =
+      "<div> item: " + id + " is deleted.</div>";
+  }
+};
 
-
-//open button
-const openes = document.querySelectorAll('.open-btn')
-const desBoxes = document.querySelectorAll('description-container')
-
-openes.forEach(function (btn) {
-    btn.addEventListener('click', function(){
-        desBoxes.classList.remove('hidden');
-    })
-})
-
+//Calendar
 new Calendar({
-    id: '#color-calendar',
-})
+  id: "#color-calendar",
+});
